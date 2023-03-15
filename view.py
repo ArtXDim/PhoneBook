4bd2c26558e1aab87827dc60e76f50c999e4e170
Inna7777 <peshkova-inna@mail.ru>
2023-03-15 18:23:56 +0400

    Add file view

---
+++ view.py
@@ -0,0 +1,13 @@
+def greetings():
+    print('Привет')
+    
+def menu():
+    print("1 - Открыть книгу")
+    print("2 - Добавить контакт")
+    print("3 - Найти контакт")
+    print("4 - Внести изменения")
+    print("5 - Закрыть книгу")
+
+
+def find():
+    return None
