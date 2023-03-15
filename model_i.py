53be93632ba55804be11c1feec3cf026c4c80077
Inna7777 <peshkova-inna@mail.ru>
2023-03-15 23:56:42 +0400

    change menu

--- model_i.py
+++ model_i.py
@@ -1,19 +1,23 @@
 
 
-def add_contact(surname , name, phone):
-    with open ('phonenum.txt', 'a') as file:
-        file.write(f"{Фамилия} {Имя} : {Телефон}\n")
+def add_contact():
+    surname = input('Введите фамилию:')
+    name = input('Введите имя:')
+    phone = input('Введите телефон :')
+    with open ('phonenum.txt', 'a', encoding='utf-8') as file:
+        file.write(surname + ' '+ name +'   :' + phone ' ' +'\'n')
+        
 
-# Для вывода все контактов
+# Для вывода всеx контактов
 def show_contacts():
-    with open ('phonenum.txt', 'r') as file:
+    with open ('phonenum.txt', 'r', encoding = "utf-8") as file:
         for line in file:
             print(line.strip())
 
 # Для поиска контакта
 
 def find_contact(surname):
-    with open ('phonenum.txt', 'r') as file:
+    with open ('phonenum.txt', 'r', encoding = "utf-8") as file:
         for line in file:
             if surname in line or name in line:
                 return line.strip()
