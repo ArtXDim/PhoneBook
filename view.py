ceda9b464beae147a3998296524caaade17fa898
Inna7777 <peshkova-inna@mail.ru>
2023-03-15 21:17:36 +0400

    changemenu

--- view.py
+++ view.py
@@ -1,6 +1,17 @@
+fields_names = ['id', 'Имя', 'Фамилия', 'Телефон']
+
+
 def greetings():
     print('**ТЕЛЕФОННАЯ КНИГА**')
     
+# сообщения
+
+greetings = "Здравствуйте. Это Ваша телефонная книга"
+error = 'Ошибка. Проверьте  введенные данные'
+enter = 'Добавьте контакт'
+success_add = 'Контакт добавлен'
+success_save = 'Данные сохранены'
+bye = 'До свидания'
 
 def menu():
     print("1 - Показать контакты")
@@ -11,5 +22,16 @@ def menu():
     print("6 - Закрыть книгу")
 
 
+# словарь ответов
+'1': 'show'
+'2': 'add'
+'3': 'find'
+'4': 'save'
+'5': 'delet'
+'6': 'exit'
+
 def find():
     return None
+
+
+file_name = 'phonenum.txt'
