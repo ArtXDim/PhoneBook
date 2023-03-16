6abf7b42a904d750a4cc24b5d0d37b4159ed8c5f
Julia Gomozova <sunrain@yandex.ru>
2023-03-16 06:57:28 +0300

    change func name

--- controller.py
+++ controller.py
@@ -5,14 +5,18 @@ def run():
     veiw.greatings()
     while true:
         view.menu()
-        answer = input('Ответ')
+        answer = input('Ответ:')
         if answer == '1':
             data = model.read_phonebook()
             view.show_phonebook()
         elif answer == '2':
             model.add_contact()
         elif answer == '3':
-            model.find()
+            model.search_contact(input("Найти:"))
         elif answer == '4':
+            model.change_contact()
+        elif answer == '5':
+            model.delete_contact()        
+        elif answer == '6':
             break
 
