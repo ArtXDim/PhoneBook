afdae2a7de6505f1f7ff9d1396d44425fabaf4d6
Julia Gomozova <sunrain@yandex.ru>
2023-03-16 19:28:45 +0300

    add arg for functions

--- controller.py
+++ controller.py
@@ -2,21 +2,20 @@ import view
 import model
 
 def run():
-    view.greatings()
-    while true:
+    view.greetings()
+    while True:
         view.menu()
         answer = input('Ответ:')
         if answer == '1':
-            data = model.read_phonebook()
-            view.show_phonebook()
+            model.read_phonebook()
         elif answer == '2':
             model.add_contact()
         elif answer == '3':
             model.search_contact(input("Найти:"))
         elif answer == '4':
-            model.change_contact()
+            model.change_contact(input("Изменить контакт:"))
         elif answer == '5':
-            model.delete_contact()        
+            model.delete_contact(input("Удалить контакт:"))        
         elif answer == '6':
             break
 
